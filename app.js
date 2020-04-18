ee6d470e7ea7325771051f5adee3b51c973449a1
Charlotte Keddie <charlottekeddie@gmail.com>
2020-04-18 16:08:25 +0200

    stuck on checkLetter

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const start = document.querySelector('.start'); 
-const missed = 0; //if the player guesses wrong 5 times, they lose the game)
+const missed = 0;                                                                   //if the player guesses wrong 5 times, they lose the game)
 const overlay = document.getElementById('overlay');
+const heart = document.querySelector('#scoreboard ol');
 
 start.addEventListener('click', () => {
     overlay.style.display = 'none';
 });
 
 const phrases = [
-    'NOT ALL THOSE WHO WANDER ARE LOST',
-    'IT IS NEVER TOO LATE TO BE WHAT YOU MIGHT HAVE BEEN',
-    'IMAGINATION WILL GET YOU EVERYWHERE',
-    'KEEP YOUR FACE TO THE SUNSHINE AND YOU CANNOT SEE THE SHADOWS',
-    'EXPERIENCE IS SIMPLY THE NAME WE GIVE OUR MISTAKES'
+    'not all those who wander are lost',
+    'it is never too late',
+    'imagination will take you everywhere',
+    'keep your face to the sunshine',
+    'experience is simply the name we give our mistakes'
 ];
 
 
@@ -42,32 +43,45 @@ for (let i = 0; i < arr.length; i ++) {
     }
 }
 
-addPhrasetoDisplay(phraseArray); 
-
-//checkLetter.forEach((item) => console.log(index, item))
+addPhraseToDisplay(phraseArray); 
 
 const checkLetter = button => {
-    const liElements = document.getElementsByClassName('letter');
-    let match = 'null';
-    for (i = 0; i = liElements.length; i ++) {
-        if (button.textContent === liElements[i].textContent) {                 
-            liElements[i].className += 'show';
-            letterMatch = liElements[i].textContent;
-            const match = button.textContent;
+    const guesses = document.getElementsByClassName('letter');
+    let match = 0;
+    for (i = 0; i = guesses.length; i ++) {
+        console.log(guesses[i]);
+        if (guesses[i].textContent.toLowerCase() === button.toLowerCase()) {
+            guesses[i].classList.add('show');
+            match = button.textContent;
+            }
         }
         return match;
-}
-}
-
-
-qwerty.addEventListener('click', () => {
-    if (button.innerText === liElements[i].innerText) {                 
-        liElements[i].className += 'show';
-        letterMatch = liElements[i].innerText;
-        const match = button.innerText;
     }
-    
+
+qwerty.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON' && e.target.className !== 'chosen') {
+        e.target.classList.add('chosen');
+        const foundLetter = checkLetter(e.target.textContent);
+      } 
+    else if (foundLetter === null) {
+        heart.removeChild('tries');
+        missed += 1;
+    }
 });
 
- 
-    
+
+
+
+// const checkWin = () => {
+//     const haveLetter = document.getElementsByClassName('letter');
+//     const haveShow = document.getElementsByClassName('show');
+//     if (haveLetter.length === haveShow.length) {
+//         overlay.classList.add('win');
+//         overlay.style.display = 'flex';
+//         overlay.innerHTML = 'You won. Way to go!!';
+//     } else if (missed > 4) {
+//         overlay.classList.add('lose');
+//         overlay.style.display = 'flex';
+//         overlay.innerHTML = 'You lost. Better luck next time';
+//     }
+// }
